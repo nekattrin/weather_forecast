6619a5ee4fd394c28d98f271b03d536f29564059
nekattrin <negreeva.katerina@gmail.com>
2025-09-22 21:04:17 +0300

    Add dynamic temperature chart with fake data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather forecast for the next days")
 place = st.text_input("Place:")
@@ -11,3 +12,17 @@ if days == 1:
 else:
     dayDays = "days"
 st.subheader(f"{option} for the next {days} {dayDays} in {place}")
+
+
+def get_data(days):
+    dates =["2025-09-23", "2025-09-24", "2025-09-25", "2025-09-26", "2025-09-27"]
+    dates = dates[:days]
+    temperatures = [14, 15, 10, 11, 13]
+    temperatures = temperatures[:days]
+    return dates, temperatures
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "dates", "y": "temperatures(C)"},
+                 markers=True)
+st.plotly_chart(figure)
