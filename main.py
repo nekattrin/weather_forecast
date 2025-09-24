2b95c5ff2a3b32a7ff68c7bb9e2632f9c79e23eb
nekattrin <negreeva.katerina@gmail.com>
2025-09-24 19:27:01 +0300

    Add sky representation, catch wrong place error.

--- main.py
+++ main.py
@@ -2,22 +2,41 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+sky_images = {'Clear': r'C:\UcoursePy\app7_weather_forecast\sky_images\clear.png',
+              'Clouds': r'C:\UcoursePy\app7_weather_forecast\sky_images\cloud.png',
+              'Rain': r'C:\UcoursePy\app7_weather_forecast\sky_images\rain.png',
+              'Snow': r'C:\UcoursePy\app7_weather_forecast\sky_images\snow.png'}
+
 st.title("Weather forecast for the next days")
 place = st.text_input("City:")
 days = st.slider("Forecast days", min_value=1, max_value=5,
                  help="Select the number of forecasted days from 1 to 5")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
-if days == 1:
-    dayDays = "day"
-else:
-    dayDays = "days"
-st.subheader(f"{option} for the next {days} {dayDays} in {place}")
 
 
-d, t = get_data(place, days, option)
+if place:
+    if days == 1:
+        dayDays = "day"
+    else:
+        dayDays = "days"
+    st.subheader(f"{option} for the next {days} {dayDays} in {place}")
+    try:
+        filtered_data = get_data(place, days)
+        dates = [dict['dt_txt'] for dict in filtered_data]
+        if option == "Temperature":
+            temperatures = [(dict['main']['temp']-273.15) for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "dates", "y": "temperatures(C)"},
+                             markers=True)
+            st.plotly_chart(figure)
+        else:
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            images = [sky_images[condition] for condition in sky_conditions]
+            st.image(images, width=115, caption=dates)
+    except KeyError:
+        st.text(f"There is no such city as {place}, try again.")
 
 
-figure = px.line(x=d, y=t, labels={"x": "dates", "y": "temperatures(C)"},
-                 markers=True)
-st.plotly_chart(figure)
+
+
+
